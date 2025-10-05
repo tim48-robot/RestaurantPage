ba2ec8c86b2b7466a8608771ec82406dcc0fd169
tim48-robot <justintimothy303@gmail.com>
2025-10-05 16:54:23 +0700

    feat: importing function skeleton per page

--- src/index.js
+++ src/index.js
@@ -1,2 +1,33 @@
-import selamatpagi from "./page1.js";
-document.addEventListener("DOMContentLoaded", selamatpagi);
+import home from "./page1.js";
+import menu from "./menu.js";
+import contact from "./contact.js";
+import about from "./aboutus.js";
+document.addEventListener("DOMContentLoaded", home);
+
+(function listenHome(){
+    const homeBtn = document.querySelector("#home");
+    homeBtn.addEventListener("click", () => {
+        home();
+    })
+})();
+
+(function listenMenu(){
+    const menuBtn = document.querySelector("#menu");
+    menuBtn.addEventListener("click", () => {
+        menu();
+    })
+}());
+
+(function listenContact(){
+    const contactBtn = document.querySelector("#contact");
+    contactBtn.addEventListener("click", () => {
+        contact()
+    })
+}());
+
+(function listenAbout(){
+    const aboutBtn = document.querySelector("#about");
+    aboutBtn.addEventListener("click", () => {
+        about()
+    })
+}());
